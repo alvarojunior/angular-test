bec9e5ee75c54f3883943f532c6af35335c2d018
Alvaro A. Lima Jr <alvarolimajr@gmail.com>
2019-03-26 23:29:51 -0300

    small fix

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,7 +4,7 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const users = [
       { id: 1, firstName: 'Alvaro', lastName: 'Lima Jr', age: 33 },
-      { id: 2, firstName: 'Jhon', lastName: 'Kennedy', age: 87 },
+      { id: 2, firstName: 'John', lastName: 'Kennedy', age: 87 },
       { id: 3, firstName: 'Alyson', lastName: 'Kazumi', age: 34 },
       { id: 4, firstName: 'Samuel', lastName: 'Arendt', age: 45 },
       { id: 5, firstName: 'Zé', lastName: 'Bigode', age: 64 }
